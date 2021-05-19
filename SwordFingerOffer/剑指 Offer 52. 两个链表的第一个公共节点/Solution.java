414415baa1971ce19dad83461039a77a34165e43
hermi <3232752602@qq.com>
2021-05-19 08:47:18 +0800

    add 剑指 Offer 52. 两个链表的第一个公共节点

---
+++ SwordFingerOffer/剑指 Offer 52. 两个链表的第一个公共节点/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode nodeA = headA;
+        ListNode nodeB = headB;
+        while (nodeA != nodeB) {
+            if (nodeA != null) {
+                nodeA = nodeA.next;
+            } else {
+                nodeA = headB;
+            }
+            if (nodeB != null) {
+                nodeB = nodeB.next;
+            } else {
+                nodeB = headA;
+            }
+        }
+        return nodeA;
+    }
+}
