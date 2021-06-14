4c03e6ef2f1b311efcb1a6982f6f695f66e5f5f3
hermi <3232752602@qq.com>
2021-06-14 10:25:11 +0800

    add 剑指 Offer 10- I. 斐波那契数列

---
+++ SwordFingerOffer/剑指 Offer 10- I. 斐波那契数列/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    //解法一
+    public int fib(int n) {
+        int a = 0, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+    //解法二
+    public int fib(int n) {
+        int[] f = new int[n + 2];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = (f[i - 1] + f[i - 2]) % 1000000007;
+        }
+        return f[n];
+    }
+}
